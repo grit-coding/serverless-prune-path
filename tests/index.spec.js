da3b0a940e54a36de1cf6364e1014b59920822b9
grit-coding <125764176+grit-coding@users.noreply.github.com>
2023-05-28 12:04:49 +0100

    Update index.spec.js

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,7 @@
 const mockFs = require('mock-fs');
 const path = require('path');
 const fs = require('fs');
-const ServerlessPrunePath = require('../index');
+const ServerlessPrunePath = require('../src/index');
 
 describe('ServerlessPrunePath', () => {
     afterEach(() => {
