55503970ee60ef852156e584c5a556c03ff28bd7
grit-coding <125764176+grit-coding@users.noreply.github.com>
2023-06-12 20:18:54 +0100

    extract out validatePaths unit tests

--- index.spec.js
+++ index.spec.js
@@ -342,48 +342,5 @@ describe('ServerlessPrunePath', () => {
             });
         });
     });
-
-    describe('validatePaths()', () => {
-        it('should return contradictory paths when given', () => {
-            const plugin = new ServerlessPrunePath({
-                cli: { log: jest.fn() },
-                config: { servicePath: '/servicePath' },
-            });
-
-            const pathsToKeep = { all: ['path/to/keep/fileToKeep.txt', 'another/path/to/keep'] };
-            const pathsToDelete = { all: ['path/to/keep', 'another/path/to/delete'] };
-
-            const result = plugin.validatePaths(pathsToKeep, pathsToDelete);
-
-            expect(result).toEqual(['Keep: "path/to/keep/fileToKeep.txt", Delete: "path/to/keep"']);
-        });
-
-        it('should return contradictory paths within pathsToKeep', () => {
-            const plugin = new ServerlessPrunePath({
-                cli: { log: jest.fn() },
-                config: { servicePath: '/servicePath' },
-            });
-
-            const pathsToKeep = { all: ['path/to/keep/fileToKeep.txt', 'path/to/keep'] };
-            const pathsToDelete = undefined;
-
-            const result = plugin.validatePaths(pathsToKeep, pathsToDelete);
-
-            expect(result).toEqual(["Keep: \"path/to/keep/fileToKeep.txt\", Keep: \"path/to/keep\""]);
-        });
-
-        it('should return empty array when no contradictory paths are given', () => {
-            const plugin = new ServerlessPrunePath({
-                cli: { log: jest.fn() },
-                config: { servicePath: '/servicePath' },
-            });
-            const pathsToKeep = { all: ['path/to/keep/fileToKeep.txt', 'another/path/to/keep'] };
-            const pathsToDelete = { all: ['path/to/delete', 'another/path/to/delete'] };
-
-            const result = plugin.validatePaths(pathsToKeep, pathsToDelete);
-
-            expect(result).toEqual([]);
-        });
-    });
 });
 
